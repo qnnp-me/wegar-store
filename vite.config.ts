a5b046fb3e09d7f984552deff6dd1c9085582796
qnnp <58357109+qnnp-me@users.noreply.github.com>
2025-03-10 22:45:57 +0800

    feat(wegar-store): 更新网页标题并添加 React 插件- 将网页标题从 "Vite + React + TS" 修改为 "Wegar Store" - 在 package.json 中添加 @vitejs/plugin-react 依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
   plugins: [
+    react(),
     dts({
       rollupTypes: true,
       tsconfigPath: './tsconfig.app.json',
