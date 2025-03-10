9a106799c1f1690de20d4409b4c6243ee5ed98bd
qnnp <58357109+qnnp-me@users.noreply.github.com>
2025-03-10 21:30:11 +0800

    refactor(store): 重构状态管理逻辑
    - 移除 App.tsx 文件，简化项目结构
    - 重构 main.tsx 文件，实现状态管理的双向绑定
    - 更新 README.md，添加新的使用示例
    - 修改 package.json，添加 patch 发布脚本

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Wegar Store
 
 Wegar 前端状态管理 
+
+## 在 React 中
+```tsx
+import {useStore} from 'wegar-store'
+
+export const App = () => {
+  const [count, setCount] = useStore('count', 0)
+  return (
+    <>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </>
+  )
+}
+```
+
+## 其他项目中
+```ts
+import {getStore} from 'wegar-store'
+
+const {getState, subscribe} = getStore('count', 0)
+
+subscribe(store => {
+  console.log('new state:', store.state)
+})
+
+const updateState = () => {
+  getState().setState(getState().state + 1)
+}
+```
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,43 @@
 import {createRoot} from 'react-dom/client'
-import App from './App.tsx'
+import {getStore, useStore} from "./store";
+import {useEffect, useState} from "react";
+
+function App() {
+  const [state, setState] = useState(0)
+  const [store, setStore] = useStore('count', 0)
+  const {getState, subscribe} = getStore('count', 0)
+  useEffect(() => {
+    subscribe(state => {
+      setState(state.state)
+    })
+  }, []);
+  useEffect(() => {
+    getState().setState(store)
+  }, [store]);
+  return (
+    <div style={{
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      gap: '1rem',
+      fontSize: '2rem',
+    }}>
+      <div>state: {state}</div>
+      <div>store: {store}</div>
+      <button style={{
+        fontSize: '2rem',
+      }} onClick={() => setStore(store + 1)}>store +1
+      </button>
+      <button style={{
+        fontSize: '2rem',
+      }} onClick={() => getState().setState(getState().state + 1)}>common state +1
+      </button>
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')!)
   .render(<App/>)
