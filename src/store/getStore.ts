0da8600af3e19d55c2354de11b37fcdc18e34eed
qnnp <58357109+qnnp-me@users.noreply.github.com>
2025-03-20 10:24:07 +0800

    refactor(wegar-store):优化 getStore 函数并添加泛型参数
    - 在 getStore函数中添加了泛型参数 S，默认为 T[K]
    - 使用泛型参数 S 替换了函数内部的所有 T[K]，提高了类型定义的灵活性
    - 优化了 createStore 函数的类型定义，使 state 和 setState 方法的类型更加清晰

--- src/store/getStore.ts
+++ src/store/getStore.ts
@@ -2,15 +2,16 @@ import {createStore, Mutate, StoreApi, StoreMutatorIdentifier} from "zustand/van
 import {WegarStores} from "wegar-store";
 
 type WegarStore<T = unknown, Mos extends [StoreMutatorIdentifier, unknown][] = []> = Mutate<StoreApi<T>, Mos>;
-export const getStore = <T extends WegarStores = WegarStores, K extends keyof T = keyof T>(name: K, initialState?: T[K]) => {
+
+export const getStore = <T extends WegarStores = WegarStores, K extends keyof T = keyof T, S extends T[K] = T[K]>(name: K, initialState?: T[K]) => {
   globalThis.wegarStore = globalThis.wegarStore || {} as typeof globalThis.wegarStore
   if (!globalThis.wegarStore[name as string]) {
     globalThis.wegarStore[name as string] = createStore<{
-      state: T[K];
-      setState: (state: T[K] | ((state: T[K]) => T[K])) => void;
+      state: S;
+      setState: (state: S | ((state: S) => S)) => void;
     }>(
       setState => ({
-        state: initialState as T[K],
+        state: initialState as S,
         setState: (newState) => {
           setState(preState => ({
             state: typeof newState === 'function' ? (newState as CallableFunction)(preState.state) : newState
@@ -20,7 +21,7 @@ export const getStore = <T extends WegarStores = WegarStores, K extends keyof T 
     )
   }
   return globalThis.wegarStore[name as string] as WegarStore<{
-    state: T[K],
-    setState: (state: T[K] | ((state: T[K]) => T[K])) => void
+    state: S,
+    setState: (state: S | ((state: S) => S)) => void
   }>
 }
