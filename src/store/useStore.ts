7a4cb2ee6db1df0563924de0b40cd5fca811d9d5
qnnp <58357109+qnnp-me@users.noreply.github.com>
2025-03-20 10:29:12 +0800

    refactor(wegar-store):优化 useStore 钩子的类型定义
    - 在 useStore函数中添加泛型参数 S，以支持自定义状态类型
    - 更新 useState 和 setState 的类型，以确保类型一致性
    - 使用类型断言，明确指定状态的类型，提高类型安全性

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -2,12 +2,12 @@ import {useEffect, useState} from "react";
 import {getStore} from "./getStore.ts";
 import {WegarStores} from "wegar-store";
 
-export const useStore = <T extends WegarStores = WegarStores, K extends keyof T = keyof T>(name: K, initialState?: T[K]) => {
+export const useStore = <T extends WegarStores = WegarStores, K extends keyof T = keyof T, S extends T[K] = T[K]>(name: K, initialState?: T[K]) => {
   const store = getStore<T>(name, initialState)
-  const [state, setState] = useState(store.getState().state)
+  const [state, setState] = useState<S>(store.getState().state as S)
   useEffect(() => {
     store.subscribe((state) => {
-      setState(state.state)
+      setState(state.state as S)
     });
   }, []);
   return [state, store.getState().setState] as const
